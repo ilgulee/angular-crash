aaf6db96f12dc275ec805cf43ec2a11f9e0715ff
Ilgu Lee <iglee2000ca@gmail.com>
2018-08-27 12:07:36 -0400

    Add ngModel and data binding to form

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,26 +7,34 @@ import { User } from "../../models/User";
   styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: "",
+    lastName: "",
+    age: null,
+    address: {
+      street: "",
+      city: "",
+      state: ""
+    }
+  };
   users: User[];
-  showExtended:boolean=true;
-  loaded:boolean=false;
-  enableAddUser:boolean=false;
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enableAddUser: boolean = false;
+  showAddForm: boolean = false;
   constructor() {
     this.users = [];
     console.log(this.users);
   }
 
   ngOnInit() {
-    setTimeout(()=>{
+    setTimeout(() => {
       this.loadUsers();
-      this.loaded=true;
-    },2000);
-
-
-
+      this.loaded = true;
+    }, 2000);
   }
 
-  loadUsers(){
+  loadUsers() {
     this.users.push({
       firstName: "Ilgu",
       lastName: "Lee",
@@ -36,22 +44,22 @@ export class UsersComponent implements OnInit {
         city: "Toronto",
         state: "ON"
       },
-      registered:new Date(),
-      isActive:true,
-      hide:true
+      registered: new Date(),
+      isActive: true,
+      hide: true
     });
     this.users.push({
       firstName: "Samho",
       lastName: "Park",
-       age: 39,
+      age: 39,
       address: {
         street: "Yonge",
         city: "Seoul",
         state: "BC"
       },
-      isActive:false,
-      registered:new Date(),
-      hide:true
+      isActive: false,
+      registered: new Date(),
+      hide: true
     });
     this.users.push({
       firstName: "Doim",
@@ -62,22 +70,22 @@ export class UsersComponent implements OnInit {
         city: "Kwangju",
         state: "PS"
       },
-      isActive:true,
-      registered:new Date(),
-      hide:true
+      isActive: true,
+      registered: new Date(),
+      hide: true
     });
     this.users.push({
       firstName: "Yeonhee",
       lastName: "Seo",
       age: 40,
       address: {
-        street: "North York",
+        street: "North York"
         // city: "Mokpo",
         // state: "Jeolla"
       },
-      isActive:true,
-      registered:new Date(),
-      hide:true
+      isActive: true,
+      registered: new Date(),
+      hide: true
     });
     // this.addUser({
     //   firstName: "Simpo",
@@ -90,11 +98,33 @@ export class UsersComponent implements OnInit {
     //   }
     // });
   }
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+    this.user.hide = true;
+    this.users.unshift(this.user);
+    this.user = {
+      firstName: "",
+      lastName: "",
+      age: null,
+      address: {
+        street: "",
+        city: "",
+        state: ""
+      }
+    };
   }
 
-  toggle(user:User){
-    user.hide=!user.hide;
+  toggle(user: User) {
+    user.hide = !user.hide;
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  fireEvent(e) {
+    console.log(e.type);
+    console.log(e.target.value);
   }
 }
